92394a21a222fc6a3d4130bddf0b2a9482678637
itsironhawk <itsironhawk@gmail.com>
2015-11-19 09:12:51 -0500

    adds function

--- server.rb
+++ server.rb
@@ -1,15 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-  puts "What is your name?"
-  name = gets
-  name = name.chomp
-  puts "Hello, " + name + ". How many ingredients do you have?"
-  variables = gets.chomp
-  puts "Which " + variables + " ingredients do you have, " + name + "?"
-  puts "List your first ingredient."
-  ingredient1 = gets.chomp
-  puts "With " + ingredient1 + " you can make:"
+#empty variables
+name = ""
+ingredients = Array.new
+ingredientNumber = ""
+currentIngredient = 1
 
+
+#get user's name
+puts "What is your name?"
+name = gets.chomp
+puts "Hello, " + name + "."
+
+#asks for number of ingredients
+puts "How many ingredients do you have?"
+ingredientNumber = gets.chomp.to_i
+
+#asks for more ingredients
+until currentIngredient == ingredientNumber do
+  puts "What is an ingredient that you have?"
+  ingredients << gets.chomp
+  ingredients = ingredients
+  currentIngredient = currentIngredient + 1
+end
+
+if currentIngredient == ingredientNumber
+  puts "With " + ingredients.to_s + " you can make:"
 end
